9d41b26482653d81b0072bbd3e8442a027563c80
Lia Kitamura <kit4mura@gmail.com>
2017-11-29 18:18:32 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+genre_options = ['Pop', 'Rock', 'Country', 'International', 'EDM', 'Hip Hop', 'Latin']
+
+20.times do |name|
+  Artist.create(name: Faker::RickAndMorty.character)
+end
+
+20.times do |name, duration, genre, artist_id|
+  Song.create(
+    name: Faker::Movie.quote,
+    duration: Faker::Number.decimal(3, 5),
+    genre: genre_options.sample,
+    artist_id: rand(1..20))
+end
